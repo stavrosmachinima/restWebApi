9f10f312eda75ec96bbef4ce474100c53540cd19
stpeppas41@gmail.com <stpeppas41@gmail.com>
2023-01-08 20:37:37 +0200

    Final

--- src/main/java/com/stve/restwebapi/controller/MyController.java
+++ src/main/java/com/stve/restwebapi/controller/MyController.java
@@ -5,8 +5,6 @@ import com.stve.restwebapi.service.MatchOdssService;
 import com.stve.restwebapi.service.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
 import java.util.List;
 
 @RestController
